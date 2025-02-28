4de2ef6ff792ad79a71bd23e63da803a11e29615
Aline-Aguiar <aline.aglilly@gmail.com>
2025-02-27 21:42:47 -0300

    incluindo arquivo script.js e setando função na div item-resposta

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const perguntas = document.querySelectorAll('.pergunta');
+
+perguntas.forEach(pergunta => {
+    pergunta.addEventListener('click', () => {
+        const resposta = pergunta.querySelector('.item-resposta');
+        resposta.classList.toggle('ativo');
+    })
+})
